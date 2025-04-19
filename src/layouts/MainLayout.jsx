885e212a93a27816e6371d3b3a6e15eba4f8f7d4
md-zeon <zeonrahaman5870@gmail.com>
2025-04-19 19:14:59 +0600

    Added Footer and images

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,16 +1,18 @@
-import React from 'react';
-import { Outlet } from 'react-router';
-import Navbar from '../components/Navbar';
-import Footer from '../components/Footer';
+import React from "react";
+import { Outlet } from "react-router";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
 
 const MainLayout = () => {
-    return (
-        <>
-         <Navbar /> 
-         <Outlet />
-         <Footer />
-        </>
-    );
+	return (
+		<>
+			<Navbar />
+			<div className="min-h-[calc(100vh-7.25rem)]">
+				<Outlet />
+			</div>
+			<Footer />
+		</>
+	);
 };
 
 export default MainLayout;
